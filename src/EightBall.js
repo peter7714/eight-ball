83bad4274d0330eb4feaa7b0758ebef9f59ce306
peter <peter.26cast@gmail.com>
2023-04-20 20:22:18 -0400

    /

--- src/EightBall.js
+++ src/EightBall.js
@@ -7,7 +7,8 @@ const EightBall = () => {
     const randAnswer = () => {return Math.floor(Math.random() * Answers.length)}
     return (
         <>
-            <h1>Magic Eight Ball!</h1>
+            <h1>Magic Eight Ball</h1>
+            <hr></hr>
             <div onClick={() => setAnswer(Answers[randAnswer()])} id="eight-ball" className={answer.color}>
                 <div className="wrapper-eight-ball-answer">
                     <p className="eight-ball-answer">{answer.msg}</p>
